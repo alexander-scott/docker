a9236faad448ac64a13b41aa19884477f1cda15f
CI <mihaigalos@gmail.com>
2021-01-27 19:39:36 +0100

    Reenable Buildkit

--- .github/workflows/build_and_push.sh
+++ .github/workflows/build_and_push.sh
@@ -3,12 +3,12 @@
 set -eoux
 
 function build() {
-    #if [ -z "${DOCKERFILE_SUFFIX}" ]; then
-    #    time DOCKER_BUILDKIT=1 docker build . --file Dockerfile$DOCKERFILE_SUFFIX ${PLATFORM} --tag $IMAGE_NAME
-    #else 
+    if [ -z "${DOCKERFILE_SUFFIX}" ]; then
+        time DOCKER_BUILDKIT=1 docker build . --file Dockerfile$DOCKERFILE_SUFFIX ${PLATFORM} --tag $IMAGE_NAME
+    else 
         # Buildkit cannot cross-build for i.e. arm32, arm64
         time docker build . --file Dockerfile$DOCKERFILE_SUFFIX ${PLATFORM} --tag $IMAGE_NAME
-    #fi
+    fi
 }
 
 function set_variables() {
